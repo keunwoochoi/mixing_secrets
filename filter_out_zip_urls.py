c6785cecaf2b2909d91b15b016ff83b77e1c038e
Keunwoo Choi <keunwoo.choi@bytedance.com>
2021-03-17 09:10:48 -0400

    update url 2020-03-17

---
+++ filter_out_zip_urls.py
@@ -0,0 +1,17 @@
+import re
+import ipdb
+
+with open('page_source.txt') as f_txt:
+    lines = f_txt.readlines()
+
+lines = ' '.join(lines)
+
+urls = re.findall(r'(?:http\:|https\:)?\/\/.*\.(?:zip|zip)', lines)
+urls = [url for url in urls if 'Full.zip' in url]
+
+print('%d urls were found' % len(urls))
+
+with open('zip_urls.txt', 'w') as f:
+    for url in urls:
+        f.write(url + '\n')
+
