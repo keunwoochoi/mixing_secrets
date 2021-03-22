46b70dde99f6f110f0e924f21660d8d68a0f0f06
Keunwoo Choi <keunwoo.choi@bytedance.com>
2021-03-22 19:45:05 -0400

    add unzipping vocal files

---
+++ unzip_vocals_only.py
@@ -0,0 +1,45 @@
+"""This script loads file names of vocal stems from `filenames_vocal.json`.
+It unzips and moves them into a single directory.
+"""
+import os
+import json
+import shutil
+import zipfile
+import argparse
+import tempfile
+
+json_path = 'filenames_vocal.json'
+
+
+def main(source_path, target_path):
+    with open(json_path, 'r') as f:
+        data = json.load(f)  # zip filename: list of member paths
+
+    # validate if there's no same file name because we'll flatten it
+    # filepaths = []
+    # for _, members in data.items():
+    #     filepaths.extend([f.split('/')[-1] for f in members])
+    #
+    # assert len(set(filepaths)) == len(filepaths)
+    # ..ok there are overlapping names, so..  i'll need to be more careful as below.
+    
+    temp_dir = tempfile.TemporaryDirectory()
+
+    for zipfile, subpaths in data.items():
+        with zipfile.ZipFile(os.path.join(source_path, zipfile)) as zip_ref:
+            for subpath in subpaths:
+                zip_ref.extract(subpath, temp_dir)
+                new_filename = subpath.replace('/', '-')
+                shutil.move(os.path.join(temp_dir, subpath), os.path.join(target_path, new_filename))
+
+    temp_dir.cleanup()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source_path', type=str, required=True, help='path that has all the zip files.')
+    parser.add_argument('--target_path', type=str, required=True, help='path to decompress the vocal stem wavs.')
+
+    args = parser.parse_args()
+
+    main(args.source_path, args.target_path)
